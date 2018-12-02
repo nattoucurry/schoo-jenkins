d8aa03e1fc9a9805c7f314c1558967346a8b63c7
nattoucurry <nattou_curry@yahoo.co.jp>
2018-12-02 22:27:17 +0900

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -6,4 +6,4 @@ BASE_DIR=$(cd $(dirname $0); pwd)
 cd ${BASE_DIR}
 
 mkdir -p ${BASE_DIR}/target/classes
-javac -cp "${BASE_DIR}/lib/*" -d ${BASE_DIR}/target/classes $(find src/{main,test}/java/schoo/jenkins -name "*.java")
+javac -source 7 -target 7 -cp "${BASE_DIR}/lib/*" -d ${BASE_DIR}/target/classes $(find src/{main,test}/java/schoo/jenkins -name "*.java")
